0820355630d97c919dff9d3b9753b91efa827406
Gabriel Roks <gabrielrokssz@gmail.com>
2025-08-03 17:20:30 -0300

    Update aula61.py

--- Basic_module/aula61.py
+++ Basic_module/aula61.py
@@ -30,15 +30,15 @@ multiplicador = 10
 resultado_digito_1 = 0
 multiplicador_2 = 11
 resultado_digito_2 = 0
-for digito_1 in nove_digitos:
-    resultado_digito_1 += int(digito_1) * multiplicador
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * multiplicador
     multiplicador -= 1
 digito_1 = (resultado_digito_1 * 10)% 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
 print(digito_1)
 
-for digito_2 in dez_digitos:
-    resultado_digito_2 += int(digito_2) * multiplicador_2
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * multiplicador_2
     multiplicador_2 -= 1
 digito_2 = (resultado_digito_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
