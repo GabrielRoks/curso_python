b71e6e3c2a60aadc3993c6bbe5713b351a331f53
Gabriel Roks <gabrielrokssz@gmail.com>
2025-08-05 20:22:50 -0300

    funções

--- intermediary_module/aula062.py
+++ intermediary_module/aula062.py
@@ -11,3 +11,13 @@ Por padrão, funções em Python retornam None (nada)
 def saudacao(nome="Sem nome"): # usando o valor 'Sem nome' para caso o usuário não defina um argumento
     print(f'Olá, {nome}!')
 saudacao("Gabriel")
+
+def multiplo_de(numero, multiplo):
+    resultado = numero % multiplo == 0
+    print(f'{numero} é múltiplo de {multiplo}?', end=' ')
+    print(resultado)
+ 
+ 
+multiplo_de(16, 8)
+multiplo_de(15, 3)
+multiplo_de(10, 2)
