8486ca73f62e291503e87ff0f83935c133209d1d
Gabriel Roks <gabrielrokssz@gmail.com>
2025-08-05 21:21:12 -0300

    Update aula063.py

--- intermediary_module/aula063.py
+++ intermediary_module/aula063.py
@@ -3,3 +3,6 @@ Argumentos nomeados e não nomeados em função Python
 Argumento nomeado tem nome com sinal de igual 
 Argumento não nomeado recebe apenas o argumento (valor)
 """
+def soma(x, y):
+    print(f'{x= } {y= }', '|', 'x + y = ', x + y)
+soma(1, 2)
