b1eb56b2a2f30072aabaa54650ce256d2bb7041b
Gabriel Roks <gabrielrokssz@gmail.com>
2025-09-18 18:32:29 -0300

    Update aula069.py

--- intermediary_module/aula069.py
+++ intermediary_module/aula069.py
@@ -1,4 +1,20 @@
 """
-Higher order functions
+Higher Order Functions
 Funções de primeira classe
 """
+
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}!'
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+print(
+    executa(saudacao, 'Bom dia', 'Luiz')
+)
+print(
+    executa(saudacao, 'Boa noite', 'Maria')
+)
